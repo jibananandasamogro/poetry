d75a02981dcd1c00a66773f94d6043f6f0408552
Utsargo Roy <royutsargo@gmail.com>
2020-05-14 20:46:36 +0600

    added files

---
+++ _book/publish.sh
@@ -0,0 +1,51 @@
+# install the plugins and build the static site
+rm _book/জীবনানন্দ-দাশের-কবিতাসমগ্র.pdf
+
+rm _book/জীবনানন্দ-দাশের-কবিতাসমগ্র.epub
+
+rm _book/জীবনানন্দ-দাশের-কবিতাসমগ্র.mobi
+
+gitbook install && gitbook build
+
+rm জীবনানন্দ-দাশের-কবিতাসমগ্র.pdf
+
+rm জীবনানন্দ-দাশের-কবিতাসমগ্র.epub
+
+rm জীবনানন্দ-দাশের-কবিতাসমগ্র.mobi
+# Generate a PDF file
+gitbook pdf ./ ./জীবনানন্দ-দাশের-কবিতাসমগ্র.pdf
+
+# Generate an ePub file
+gitbook epub ./ ./জীবনানন্দ-দাশের-কবিতাসমগ্র.epub
+
+# Generate a Mobi file
+gitbook mobi ./ ./জীবনানন্দ-দাশের-কবিতাসমগ্র.mobi
+
+
+# checkout to the gh-pages branch
+git checkout master
+
+# pull the latest updates
+git pull origin master --rebase
+
+# copy the static site files into the current directory.
+cp -R _book/* .
+
+cd ../
+cp favicon.ico gitbook/images
+cp apple-touch-icon-precomposed-152.png gitbook/images
+# remove 'node_modules' and '_book' directory
+git clean -fx node_modules
+git clean -fx _book
+
+# add all files
+git add .
+
+# commit
+git commit -a -m "Update Book"
+
+# push to the origin
+git push -u origin master
+
+# checkout to the master branch
+git checkout master
