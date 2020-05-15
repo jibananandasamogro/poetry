44ad8f6ab102b7b82a5d1d89aa0f483b5c1ec912
Utsargo Roy <royutsargo@gmail.com>
2020-05-16 04:12:44 +0600

    Update Book

--- gitbook/gitbook-plugin-scripts/95560520215bc04ef263c5a0983d9a68-custom.js
+++ gitbook/gitbook-plugin-scripts/95560520215bc04ef263c5a0983d9a68-custom.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $("head").append("<meta property='og:image' content='https://xn--w5bufaav1kna6j.xn--54b7fta0cc/images/jibanananda.jpg' />");
+    $("head").append("<meta property='og:image' content='/jibanananda.jpg' />");
 });
