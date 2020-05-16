60421cfcc01b9305125fff3b7c3e7f9f67079206
Utsargo Roy <royutsargo@gmail.com>
2020-05-17 02:02:22 +0600

    Update Book

--- publish.sh
+++ publish.sh
@@ -23,7 +23,7 @@ git checkout master
 git pull origin master --rebase
 
 # copy the static site files into the current directory.
-cp -R _book/*.html .
+cp -R _book/ .
 # rsync -av --progress _book . --exclude styles
 
 cp favicon.ico gitbook/images
